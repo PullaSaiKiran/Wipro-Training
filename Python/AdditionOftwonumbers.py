b6881ed58c318dae50d4c135380eb60b279f8b6d
PullaSaiKiran <saikiranp816@gmail.com>
2026-04-21 18:28:57 +0530

    First Programs

---
+++ Python/AdditionOftwonumbers.py
@@ -0,0 +1,4 @@
+#Addition of Two Numbers
+First_Number=int(input("Enter the First Number:"))
+Second_Number=int(input("Enter the Second Number:"))
+print("Addition of Two Numbers", First_Number+Second_Number)
---
+++ Python/Clicommands.py
@@ -0,0 +1,47 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=============== RESTART: C:/Wipro Training/Python/firstprogram.py ==============
+Traceback (most recent call last):
+  File "C:/Wipro Training/Python/firstprogram.py", line 1, in <module>
+    Print("Hello World!!!")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+=============== RESTART: C:/Wipro Training/Python/firstprogram.py ==============
+Hello World!!!
+
+=========== RESTART: C:/Wipro Training/Python/AdditionOftwonumbers.py ==========
+Enter the First Number:10
+Enter the Second Number:20
+Addition of Two Numbers 30
+>>> x=10
+>>> x
+10
+>>> type(x)
+<class 'int'>
+>>> s=''
+>>> s
+''
+>>> type(s)
+<class 'str'>
+>>> f=10
+>>> f
+10
+>>> f=10.2
+>>> f
+10.2
+>>> type(f)
+<class 'float'>
+>>> c=2+1j
+>>> c
+(2+1j)
+>>> type(c)
+<class 'complex'>
+>>> b=True
+>>> b
+True
+>>> b2=False
+>>> b2
+False
+>>> type(b2)
+<class 'bool'>
