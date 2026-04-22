fae2772181cdf18cf13d84eed285d775e8ff1470
PullaSaiKiran <saikiranp816@gmail.com>
2026-04-22 11:48:56 +0530

    first

---
+++ Python/Python coding/Calculation of Numbers.py
@@ -0,0 +1,8 @@
+#Calculationoftwonumbers
+First_Number=int(input("Enter the First Number:"))
+Second_Number=int(input("Enter the Second Number:"))
+print("Addition of Two Numbers", First_Number+Second_Number)
+print("Subtraction of Two Numbers", First_Number-Second_Number)
+print("Mutiple of Two Numbers", First_Number*Second_Number)
+print("Division of Two Numbers", First_Number/Second_Number)
+print("Mod of Two Numbers", First_Number%Second_Number)
