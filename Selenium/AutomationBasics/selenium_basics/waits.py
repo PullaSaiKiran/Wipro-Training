f991436543f8968833d83db90b137cf6ebbb924a
PullaSaiKiran <saikiranp816@gmail.com>
2026-05-04 18:34:07 +0530

    selenium

--- Selenium/AutomationBasics/selenium_basics/waits.py
+++ Selenium/AutomationBasics/selenium_basics/waits.py
@@ -16,6 +16,32 @@ driver.get("https://www.google.com")
 #
 # googlesearch_button = driver.find_element(By.NAME,'btnK')
 # googlesearch_button.click()
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.edge.service import Service
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.keys import Keys
+#
+# driver = webdriver.Edge(service=Service("../resources/msedgedriver.exe"))
+# driver.get("https://www.google.com")
+#
+# wait = WebDriverWait(driver, 10)
+#
+# # Wait for search box
+# search_box = wait.until(EC.visibility_of_element_located((By.NAME, "q")))
+# search_box.send_keys("Explicit Wait")
+#
+# # Option 1: Wait for button to be clickable
+# search_button = wait.until(EC.element_to_be_clickable((By.NAME, "btnK")))
+# search_button.click()
+#
+# # Option 2 (simpler): just press Enter instead of clicking
+# # search_box.send_keys(Keys.RETURN)
+#
+# time.sleep(5)
+# driver.quit()
 
 import time
 from selenium import webdriver
@@ -23,19 +49,26 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Edge(service=Service("../resources/msedgedriver.exe"))
 driver.get("https://www.google.com")
 
-wait = WebDriverWait(driver, 10)
+# Fluent Wait setup
+wait = WebDriverWait(
+    driver,
+    timeout=15,              # maximum wait time
+    poll_frequency=2,        # check every 2 seconds
+    ignored_exceptions=[NoSuchElementException]  # ignore missing element errors
+)
 
 # Wait for search box
 search_box = wait.until(EC.visibility_of_element_located((By.NAME, "q")))
-search_box.send_keys("Implicit Wait")
+search_box.send_keys("Fluent Wait in Selenium")
 
 # Wait for the Google Search button to be clickable
-googlesearch_button = wait.until(EC.element_to_be_clickable((By.NAME, "btnK")))
-googlesearch_button.click()
+search_button = wait.until(EC.element_to_be_clickable((By.NAME, "btnK")))
+search_button.click()
 
 time.sleep(5)
 driver.quit()
